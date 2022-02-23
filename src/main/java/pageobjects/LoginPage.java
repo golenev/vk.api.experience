f8dea73e72338a1e3858e71b4140c6cedf6ecaad
Basile <interbeing34@gmail.com>
2022-02-23 21:30:57 +0300

    first changes

--- src/main/java/pageobjects/LoginPage.java
+++ src/main/java/pageobjects/LoginPage.java
@@ -1,11 +1,27 @@
 package pageobjects;
 
+import aquality.selenium.elements.interfaces.IButton;
+import aquality.selenium.elements.interfaces.ITextBox;
 import aquality.selenium.forms.Form;
 import org.openqa.selenium.By;
 
 public class LoginPage extends Form {
 
-    protected LoginPage(By locator, String name) {
-        super(locator, name);
+    private ITextBox loginField = getElementFactory().getTextBox(By.cssSelector("#index_email"), "field for login");
+    private ITextBox passwordField = getElementFactory().getTextBox(By.cssSelector("#index_pass"), "field for password");
+    private IButton loginBtn = getElementFactory().getButton(By.cssSelector("#index_login_button"), "login button");
+
+    public LoginPage() {
+        super(By.cssSelector("#index_login"), "authorization window");
     }
+    public void setLoginField (String login){
+        loginField.clearAndType(login);
+    }
+    public void setPasswordField (String password) {
+        passwordField.clearAndType(password);
+    }
+    public void loginButtonClick(){
+        loginBtn.click();
+    }
+
 }
--- src/test/java/AllTests.java
+++ src/test/java/AllTests.java
@@ -1,10 +1,17 @@
 import aquality.selenium.browser.AqualityServices;
 import org.testng.annotations.Test;
+import pageobjects.LoginPage;
 
 public class AllTests extends BaseTest{
+    LoginPage loginPage = new LoginPage();
+
+
     @Test
     public void firstTest(){
         browser = AqualityServices.getBrowser();
         browser.goTo("https://vk.com/");
+        loginPage.setLoginField("+79054825726");
+        loginPage.setPasswordField("Volgograd!23");
+        loginPage.loginButtonClick();
     }
 }
